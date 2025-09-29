ced9864a54f73187bcf9c44dca28cf65a5fba9b9
chillfish8 <hburt2003@gmail.com>
2025-09-29 22:13:39 +0100

    Test missed debug impls

--- src/layout/tests/mod.rs
+++ src/layout/tests/mod.rs
@@ -1,6 +1,6 @@
 use chacha20poly1305::{Key, KeyInit, XChaCha20Poly1305};
 
-use crate::layout::encrypt;
+use crate::layout::{PageFileId, PageGroupId, PageId, encrypt};
 
 mod file_metadata;
 mod log;
@@ -14,4 +14,11 @@ fn cipher_1() -> encrypt::Cipher {
 fn cipher_2() -> encrypt::Cipher {
     let key = Key::from_slice(b"8f4935bDBd0A771bA20fda47f44bf2bf");
     XChaCha20Poly1305::new(key)
+}
+
+#[test]
+fn test_id_displays() {
+    assert_eq!(format!("{:?}", PageId(1)), "PageId(1)");
+    assert_eq!(format!("{:?}", PageGroupId(2)), "PageGroupId(2)");
+    assert_eq!(format!("{:?}", PageFileId(3)), "PageFileId(3)");
 }
