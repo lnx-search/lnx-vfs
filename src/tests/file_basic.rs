651c81b0782d29920ceee77bee3efb93d53039a3
chillfish8 <hburt2003@gmail.com>
2025-10-10 00:49:43 +0100

    Test truncate and allocate functions

--- src/tests/file_basic.rs
+++ src/tests/file_basic.rs
@@ -62,3 +62,44 @@ async fn test_file_write(tempdir: tempfile::TempDir) {
     let buffer = std::fs::read(file_path).unwrap();
     assert_eq!(&buffer[..13], b"Hello, world!");
 }
+
+
+#[rstest::rstest]
+#[tokio::test]
+async fn test_file_allocate(tempdir: tempfile::TempDir) {
+    let directory = SystemDirectory::open(tempdir.path())
+        .await
+        .expect("directory should be created");
+
+    let file_id = directory.create_new_file(FileGroup::Pages).await.unwrap();
+    let file = directory.get_rw_file(FileGroup::Pages, file_id).await.unwrap();
+
+    file.allocate(0, 4096)
+        .await
+        .expect("file should allocate 4kB");
+    let len = file.get_len().await.unwrap();
+    assert_eq!(len, 4096);
+}
+
+#[rstest::rstest]
+#[tokio::test]
+async fn test_file_truncate(tempdir: tempfile::TempDir) {
+    let directory = SystemDirectory::open(tempdir.path())
+        .await
+        .expect("directory should be created");
+
+    let file_id = directory.create_new_file(FileGroup::Pages).await.unwrap();
+    let file = directory.get_rw_file(FileGroup::Pages, file_id).await.unwrap();
+
+    file.truncate(4096)
+        .await
+        .expect("file should allocate 4kB");
+    let len = file.get_len().await.unwrap();
+    assert_eq!(len, 4096);
+
+    file.truncate(0)
+        .await
+        .expect("file should allocate 4kB");
+    let len = file.get_len().await.unwrap();
+    assert_eq!(len, 0);
+}
