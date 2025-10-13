65a7468ac9e538282992286ff43eed98979d0e3c
chillfish8 <hburt2003@gmail.com>
2025-10-13 21:07:32 +0100

    Adjust ticket system to more reliably track oldest alive ticket

--- src/cache/mem_block/ticket.rs
+++ src/cache/mem_block/ticket.rs
@@ -5,7 +5,7 @@ use std::sync::atomic::{AtomicU64, Ordering};
 use arc_swap::ArcSwap;
 use parking_lot::Mutex;
 
-const TICKETS_PER_GENERATION: u64 = 256;
+const APPROX_TICKETS_PER_GENERATION: u64 = 256;
 
 /// The generation handler keeps track of active references
 /// to pages over time.
@@ -28,9 +28,10 @@ pub(super) struct GenerationTicketMachine {
     ticket_counter: AtomicU64,
     /// The current generation in use for new tickets.
     ///
-    /// Since this uses a [MiniArcSwap] tickets _might_ be put with an
-    /// older generation, but this is fine as we still uphold
-    /// the guarantee that older tickets are dropped.
+    /// The accesses to this do not need to be strongly consistent,
+    /// meaning newer ops are allowed to get a reference to an older generation,
+    /// all it means is it defers at what point the oldest alive ticket
+    /// is advanced slightly.
     active_generation: ArcSwap<GenerationEntry>,
     /// State shared with [TicketGuard]s so they can automatically
     /// advance the generation state when dropped.
@@ -42,22 +43,25 @@ impl Default for GenerationTicketMachine {
         let shared_state = Arc::new(SharedState {
             oldest_alive_ticket: AtomicU64::new(0),
             generation: Mutex::new(GenerationState {
-                next_generation_id: 0,
-                active_generations: BTreeSet::new(),
+                known_active_tickets: BTreeSet::new(),
             }),
         });
 
         // This entry is immediately replaced, always.
         let base_entry = GenerationEntry {
-            generation_id: u64::MAX,
+            oldest_ticket: 0,
             shared_state: shared_state.clone(),
         };
 
-        Self {
+        let this = Self {
             ticket_counter: AtomicU64::new(0),
             active_generation: ArcSwap::from_pointee(base_entry),
             shared_state,
-        }
+        };
+        // We always force a rotation to fix the one possible divergence which is the
+        // first "oldest_ticket" the ticket machine will see, will be 1 instead of 0.
+        this.increment_ticket_id();
+        this
     }
 }
 
@@ -74,7 +78,7 @@ impl GenerationTicketMachine {
     pub(super) fn increment_ticket_id(&self) -> u64 {
         let ticket_id = self.ticket_counter.fetch_add(1, Ordering::Relaxed);
 
-        if ticket_id.is_multiple_of(TICKETS_PER_GENERATION) {
+        if ticket_id.is_multiple_of(APPROX_TICKETS_PER_GENERATION) {
             self.advance_generation();
         }
 
@@ -82,14 +86,24 @@ impl GenerationTicketMachine {
     }
 
     /// Replace the current active generation with the next generation.
-    pub(super) fn force_advance_generation(&self) {
-        self.ticket_counter
-            .fetch_add(TICKETS_PER_GENERATION, Ordering::Relaxed);
-        self.advance_generation()
-    }
-
-    fn advance_generation(&self) {
-        let generation = self.shared_state.replace_generation();
+    pub(super) fn advance_generation(&self) {
+        // Note that it is safe to increment the ticket counter here, even if we are
+        // advancing the generation because we hit the `APPROX_TICKETS_PER_GENERATION`
+        // threshold.
+        //
+        // This is because how we calculate the oldest ticket is respective of the generation
+        // ID and is instead solely based on the `oldest_ticket` defined within the generation
+        // which is this value we load.
+        //
+        // This means even if we force an advance on ticket 255 and the next increment
+        // hits 256 and rotates the generation again, we do not end up in a situation where
+        // the oldest alive ticket is ever larger than the possibly alive tickets.
+        //
+        // That being said, double advances can be a little bit inefficient, but I don't believe
+        // it will be a large issue and can always be optimised by not forcing an advance
+        // as much.
+        let oldest_ticket = self.ticket_counter.fetch_add(1, Ordering::Relaxed);
+        let generation = self.shared_state.replace_generation(oldest_ticket);
         self.active_generation.store(generation);
     }
 
@@ -97,8 +111,8 @@ impl GenerationTicketMachine {
     /// which will ensure the oldest ticket counter is not advanced until
     /// this guard is dropped.
     pub(super) fn get_next_ticket(&self) -> TicketGuard {
-        self.increment_ticket_id();
         let generation = self.active_generation.load_full();
+        self.increment_ticket_id();
         TicketGuard { generation }
     }
 }
@@ -143,13 +157,12 @@ impl SharedState {
         self.oldest_alive_ticket.store(ticket, Ordering::Relaxed);
     }
 
-    fn replace_generation(self: &Arc<Self>) -> Arc<GenerationEntry> {
+    fn replace_generation(self: &Arc<Self>, oldest_ticket: u64) -> Arc<GenerationEntry> {
         let shared_state = self.clone();
         let mut lock = self.generation.lock();
 
-        let generation_id = lock.next_generation_id();
         let entry = GenerationEntry {
-            generation_id,
+            oldest_ticket,
             shared_state,
         };
 
@@ -158,8 +171,7 @@ impl SharedState {
 }
 
 struct GenerationState {
-    next_generation_id: u64,
-    active_generations: BTreeSet<u64>,
+    known_active_tickets: BTreeSet<u64>,
 }
 
 impl GenerationState {
@@ -168,14 +180,8 @@ impl GenerationState {
         entry: GenerationEntry,
     ) -> Arc<GenerationEntry> {
         let entry = Arc::new(entry);
-        self.active_generations.insert(entry.generation_id);
+        self.known_active_tickets.insert(entry.oldest_ticket);
         entry
-    }
-
-    fn next_generation_id(&mut self) -> u64 {
-        let generation_id = self.next_generation_id;
-        self.next_generation_id += 1;
-        generation_id
     }
 }
 
@@ -184,21 +190,20 @@ impl GenerationState {
 /// When this entry is dropped it will remove itself from the `active_generations`
 /// state.
 struct GenerationEntry {
-    generation_id: u64,
+    oldest_ticket: u64,
     shared_state: Arc<SharedState>,
 }
 
 impl Drop for GenerationEntry {
     fn drop(&mut self) {
         let mut guard = self.shared_state.generation.lock();
-        guard.active_generations.remove(&self.generation_id);
+        guard.known_active_tickets.remove(&self.oldest_ticket);
 
         // The oldest generation ID is at the head of the BTree.
         let oldest_alive_ticket = guard
-            .active_generations
+            .known_active_tickets
             .first()
             .copied()
-            .map(|oldest_generation| oldest_generation * TICKETS_PER_GENERATION)
             .unwrap_or(u64::MAX);
         self.shared_state
             .set_oldest_alive_ticket(oldest_alive_ticket);
@@ -216,17 +221,16 @@ mod tests {
         let shared_state = Arc::new(SharedState {
             oldest_alive_ticket: AtomicU64::new(0),
             generation: Mutex::new(GenerationState {
-                next_generation_id: 0,
-                active_generations: BTreeSet::new(),
+                known_active_tickets: BTreeSet::new(),
             }),
         });
 
-        let gen1 = shared_state.replace_generation();
-        assert_eq!(gen1.generation_id, 0);
-        let gen2 = shared_state.replace_generation();
-        assert_eq!(gen2.generation_id, 1);
-        let gen3 = shared_state.replace_generation();
-        assert_eq!(gen3.generation_id, 2);
+        let gen1 = shared_state.replace_generation(0);
+        assert_eq!(gen1.oldest_ticket, 0);
+        let gen2 = shared_state.replace_generation(1);
+        assert_eq!(gen2.oldest_ticket, 1);
+        let gen3 = shared_state.replace_generation(2);
+        assert_eq!(gen3.oldest_ticket, 2);
 
         drop(gen2);
 
@@ -236,18 +240,12 @@ mod tests {
 
         drop(gen1);
         // now gen1 is dropped, the oldest alive ticket should become the start of
-        // the gen3 tickets aka TICKETS_PER_GENERATION * 2
-        assert_eq!(
-            shared_state.oldest_alive_ticket(),
-            TICKETS_PER_GENERATION * 2
-        );
+        // the gen3 tickets.
+        assert_eq!(shared_state.oldest_alive_ticket(), 2);
 
-        let gen4 = shared_state.replace_generation();
+        let gen4 = shared_state.replace_generation(3);
         drop(gen3);
-        assert_eq!(
-            shared_state.oldest_alive_ticket(),
-            TICKETS_PER_GENERATION * 3
-        );
+        assert_eq!(shared_state.oldest_alive_ticket(), 3);
         drop(gen4);
     }
 
@@ -256,38 +254,43 @@ mod tests {
         let machine = GenerationTicketMachine::default();
 
         let guard = machine.get_next_ticket();
-        assert_eq!(guard.generation.generation_id, 0);
 
-        machine.force_advance_generation();
+        // This should be 1 because the first fetch_add will return 0,
+        // then rotate the generation as a result and increment the ticket again
+        // resulting in 1.
+        // The ticket ID for the guard would be 2.
+        assert_eq!(guard.generation.oldest_ticket, 1);
+
+        machine.advance_generation();
         drop(guard);
 
         let guard = machine.get_next_ticket();
-        assert_eq!(guard.generation.generation_id, 1);
-        assert_eq!(machine.oldest_alive_ticket(), TICKETS_PER_GENERATION);
+        assert_eq!(guard.generation.oldest_ticket, 3);
+        assert_eq!(machine.oldest_alive_ticket(), 3);
 
         let ticket = machine.increment_ticket_id();
-        assert_eq!(ticket, 258);
+        assert_eq!(ticket, 5);
     }
 
     #[test]
     fn test_ticket_machine_increment() {
         let machine = GenerationTicketMachine::default();
 
-        assert_eq!(machine.increment_ticket_id(), 0);
-        assert_eq!(machine.increment_ticket_id(), 1);
         assert_eq!(machine.increment_ticket_id(), 2);
+        assert_eq!(machine.increment_ticket_id(), 3);
+        assert_eq!(machine.increment_ticket_id(), 4);
 
-        assert_eq!(machine.oldest_alive_ticket(), 0);
-
-        for _ in 0..256 {
-            machine.increment_ticket_id();
-        }
-        assert_eq!(machine.oldest_alive_ticket(), 256);
+        assert_eq!(machine.oldest_alive_ticket(), 1);
 
         for _ in 0..256 {
             machine.increment_ticket_id();
         }
-        assert_eq!(machine.oldest_alive_ticket(), 512);
+        assert_eq!(machine.oldest_alive_ticket(), 257);
+
+        for _ in 0..256 {
+            machine.increment_ticket_id();
+        }
+        assert_eq!(machine.oldest_alive_ticket(), 513);
     }
 
     #[test]
@@ -316,29 +319,29 @@ mod tests {
         handle2.join().unwrap();
 
         std::thread::sleep(Duration::from_millis(1));
-        assert_eq!(machine.oldest_alive_ticket(), 1792);
+        assert_eq!(machine.oldest_alive_ticket(), 2049);
     }
 
     #[test]
     fn test_ticket_guard_prevent_oldest_advancing() {
         let machine = GenerationTicketMachine::default();
-        assert_eq!(machine.increment_ticket_id(), 0);
+        assert_eq!(machine.increment_ticket_id(), 2);
 
         let guard = machine.get_next_ticket();
 
         for _ in 0..256 {
             machine.increment_ticket_id();
         }
-        assert_eq!(machine.oldest_alive_ticket(), 0);
+        assert_eq!(machine.oldest_alive_ticket(), 1);
 
         drop(guard);
-        assert_eq!(machine.oldest_alive_ticket(), 256);
+        assert_eq!(machine.oldest_alive_ticket(), 257);
     }
 
     #[test]
     fn test_threaded_ticket_guard_prevent_oldest_advancing() {
         let machine = Arc::new(GenerationTicketMachine::default());
-        assert_eq!(machine.increment_ticket_id(), 0);
+        assert_eq!(machine.increment_ticket_id(), 2);
 
         let guard = machine.get_next_ticket();
 
@@ -362,9 +365,9 @@ mod tests {
 
         handle1.join().unwrap();
         handle2.join().unwrap();
-        assert_eq!(machine.oldest_alive_ticket(), 0);
+        assert_eq!(machine.oldest_alive_ticket(), 1);
 
         drop(guard);
-        assert_eq!(machine.oldest_alive_ticket(), 2048);
+        assert_eq!(machine.oldest_alive_ticket(), 2049);
     }
 }
